8c62d78066b088431457098497b6f9ee1a1cdc86
romeo <romeo.dark@mail.ru>
2023-05-15 10:32:12 +0300

    creating rating_status function

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -6,9 +6,19 @@ from .models import Movie
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
     ordering = ['-rating', '-name']
-    list_per_page = 3
+    list_per_page = 10
+
+    def rating_status(self, mov: Movie):
+        if mov.rating < 50:
+            return 'This movie with bad reviews'
+        if mov.rating < 70:
+            return 'You can watch this movie one time'
+        if mov.rating <= 85:
+            return 'This movie is excellent'
+        return 'It\'s excellent movie'
+
 
 
