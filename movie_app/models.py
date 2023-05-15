21e311bb111fb9ae2f5ca10ef39cd09e31805621
romeo <romeo.dark@mail.ru>
2023-05-15 11:21:17 +0300

    making a choices field(for a close choice)

--- movie_app/models.py
+++ movie_app/models.py
@@ -3,18 +3,21 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 class Movie(models.Model):
+    USD = 'USD'
+    EUR = 'EUR'
+    RUB = 'RUB'
 
     CURRENCY_CHOICES = [
-        ('E', 'Euro'),
-        ('D', 'Dollar'),
-        ('R', 'Rubles'),
+        ('USD', 'Dollar'),
+        ('EUR', 'Euro'),
+        ('RUB', 'Rubles'),
     ]
 
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
-    currency = models.CharField(max_length=1, choices=CURRENCY_CHOICES, default='R')
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
 
     def save(self, *args, **kwargs):
