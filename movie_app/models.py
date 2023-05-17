317a86b9a0db3f753cadd3632fcb9f80648317d9
romeo <romeo.dark@mail.ru>
2023-05-17 12:59:21 +0300

    creating a Director class

--- movie_app/models.py
+++ movie_app/models.py
@@ -3,6 +3,14 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    director_email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Movie(models.Model):
     USD = 'USD'
     EUR = 'EUR'
@@ -22,6 +30,8 @@ class Movie(models.Model):
                                  validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
+    director = models.CharField(max_length=100, default='Luk Besson')
+    director_email = models.EmailField(default='your_boss@mail.ru')
 
     def get_url(self):
         return reverse('movie-detail', args=[self.slug])
